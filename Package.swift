e4b49fd8645cca024941624fe05e67c938447d5a
marcprux <mwp1@cornell.edu>
2022-09-02 19:21:45 -0400

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let package = Package(
     name: "WeatherTiq",
     defaultLocalization: "en",
+    platforms: [
+        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
+    ],
     products: [ .library(name: "WeatherTiq", targets: ["WeatherTiq"]) ],
     targets: [
         .target(name: "WeatherTiq", resources: [.process("Resources")]),
